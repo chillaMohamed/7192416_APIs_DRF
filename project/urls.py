c42dafdd0b49c4d85b0f94c4e6d57fe65ddf2f78
Mohamed CHILLA <chilla.mohamed06@gmail.com>
2024-04-04 11:12:51 +0200

    Rendez les Views plus génériques avec un ModelViewSet

--- project/urls.py
+++ project/urls.py
@@ -1,11 +1,19 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
 
-from shop.views import CategoryAPIView
+from shop.views import CategoryViewSet
 
+ 
+# Ici nous créons notre routeur
+router = routers.SimpleRouter()
+# Puis lui déclarons une url basée sur le mot clé ‘category’ et notre view
+# afin que l’url générée soit celle que nous souhaitons ‘/api/category/’
+router.register('category', CategoryViewSet, basename='category')
+ 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('api/category/', CategoryAPIView.as_view()),
+    path('api/', include(router.urls)),
 ]
